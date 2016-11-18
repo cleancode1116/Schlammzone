59fbe37263ce95e10c7b47650da232d73524db38
Administrator <Administrator@raum02pc01>
2016-11-18 08:46:05 +0100

    Split into abstraction methods

--- src/de/integrata/oop/sz/names/Names.java
+++ src/de/integrata/oop/sz/names/Names.java
@@ -3,18 +3,23 @@ package de.integrata.oop.sz.names;
 public class Names {
 
 	public void printPrimesUpTo(int maximumNumber) {
-		for (int potentialPrime = 1; potentialPrime < maximumNumber; potentialPrime++) { 
-		    boolean isPrime = true; 
-		    for (int divisor = 2; divisor < potentialPrime; divisor++) { 
-		        if (potentialPrime % divisor == 0) { 
-		            isPrime = false; 
-		            break; 
-		        } 
-		    } 
-		    if (isPrime) { 
-		        System.out.println(potentialPrime); 
-		    } 
-		} 
+		for (int potentialPrime = 1; potentialPrime < maximumNumber; potentialPrime++)  
+		    printIfPrime(potentialPrime); 
+	}
+
+	private void printIfPrime(int potentialPrime) {
+		if (isPrime(potentialPrime)) 
+		    System.out.println(potentialPrime); 
+	}
+
+	private boolean isPrime(int potentialPrime) {
+		for (int divisor = 2; divisor < potentialPrime; divisor++) 
+		    if (istTeilbar(potentialPrime, divisor)) return false; 
+		return true;
+	}
+
+	private boolean istTeilbar(int potentialPrime, int divisor) {
+		return potentialPrime % divisor == 0;
 	}
 	
 }
